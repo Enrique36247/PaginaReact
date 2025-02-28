f6612f62ca7a41a45bc79ace12ff6795b6cb57b9
Enrique36247 <enriqueperezgarcia@alumno.ieselrincon.es>
2025-02-28 20:34:35 +0000

    Tonteria

--- src/components/sport/Sport.jsx
+++ src/components/sport/Sport.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Sport.css';
-import { Link } from 'react-router-dom';
 import futbolImage from '../../img/futbol.jpg';
 import baloncestoImage from '../../img/basket.jpg';
 import voleibolImage from '../../img/voley.jpg';
 
 const sports = [
-    { name: 'Soccer', description: 'A team sport played between two teams of eleven players each.', image: futbolImage, path: '/futbol' },
-    { name: 'Basketball', description: 'A team sport in which two teams of five players try to score points.', image: baloncestoImage, path: '/basket' },
-    { name: 'Volleyball', description: 'A team sport in which two teams of six players face each other.', image: voleibolImage, path: '/voley' }
+    { name: 'Soccer', description: 'A team sport played between two teams of eleven players each.', image: futbolImage },
+    { name: 'Basketball', description: 'A team sport in which two teams of five players try to score points.', image: baloncestoImage },
+    { name: 'Volleyball', description: 'A team sport in which two teams of six players face each other.', image: voleibolImage }
 ];
 
 const Sport = () => {
@@ -17,9 +16,7 @@ const Sport = () => {
             {sports.map((sport, index) => (
                 <div className='sport' key={index}>
                     <img src={sport.image} alt={sport.name} className='sport-image' />
-                    <h3>
-                        <Link to={sport.path}>{sport.name}</Link>
-                    </h3>
+                    <h3>{sport.name}</h3>
                     <p>{sport.description}</p>
                 </div>
             ))}
